be188edab2bf47545d17e1ff1caf3e684dffd3a0
Soham Goswami <sohamgoswami2015@gmail.com>
2023-03-30 03:09:59 -0400

    sgetfiles and dgetfiles complete

--- client.c
+++ client.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,10 +8,87 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <time.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 #define SERVER_IP_ADDR "127.0.0.1"
+#define TAR_FILE_NAME "temp.tar.gz"
+
+// Function to handle receiving stream of file
+int receive_files(int socket_fd) {
+    // Open the tar file
+    FILE* fp = fopen(TAR_FILE_NAME, "wb");
+    if (!fp) {
+        perror("Error creating tar file");
+        return 1;
+    }
+
+    // Receive the tar file size from the server
+    char size_buffer[BUFFER_SIZE];
+    if (recv(socket_fd, size_buffer, BUFFER_SIZE, 0) == -1) {
+        perror("Error receiving tar file size from server");
+        fclose(fp);
+        return 1;
+    }
+    long tar_file_size = atol(size_buffer);
+    printf("File size received: %ld \n", tar_file_size);
+
+    sprintf(size_buffer, "%ld", tar_file_size);
+    if (send(socket_fd, size_buffer, strlen(size_buffer), 0) != strlen(size_buffer)) {
+        perror("Error acknowledging to server");
+        fclose(fp);
+        return 1;
+    }
+
+    // Receive the tar file contents from the server
+    char buffer[BUFFER_SIZE];
+    long bytes_received = 0;
+    size_t n;
+    while (bytes_received < tar_file_size && (n = recv(socket_fd, buffer, BUFFER_SIZE, 0)) > 0) {
+        if (fwrite(buffer, sizeof(char), n, fp) != n) {
+            perror("Error writing to tar file");
+            break;
+        }
+        bytes_received += n;
+    }
+    printf("File received successfully\n");
+    fclose(fp);
+    return 0;
+}
+
+// Function to parse and validate the dates passed
+int validate_dates(char* date1, char* date2) {
+    struct tm tm_date1 = {0};
+    struct tm tm_date2 = {0};
+    time_t time_date1, time_date2;
+
+    if (strptime(date1, "%Y-%m-%d", &tm_date1) == NULL) {
+        printf("Failed to parse date string: %s\n", date1);
+        return 1;
+    }
+    if (strptime(date2, "%Y-%m-%d", &tm_date2) == NULL) {
+        printf("Failed to parse date string: %s\n", date2);
+        return 1;
+    }
+
+    time_date1 = mktime(&tm_date1);
+    time_date2 = mktime(&tm_date2);
+
+    if (time_date1 > time_date2) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void send_command(int client_fd, char* buffer) {
+    // Send command to server
+    if (send(client_fd, buffer, strlen(buffer), 0) != strlen(buffer)) {
+        perror("TCP Client - Send Error");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(int argc, char const *argv[]) {
     int client_fd, n;
@@ -54,30 +133,62 @@ int main(int argc, char const *argv[]) {
         fgets(buffer, BUFFER_SIZE, stdin);
         buffer[strlen(buffer)-1] = '\0';
 
-        // Send command to server
-        if (send(client_fd, buffer, strlen(buffer), 0) != strlen(buffer)) {
-            perror("TCP Client - Send Error");
-            exit(EXIT_FAILURE);
-        }
-
         // Exit if user types "quit"
         if (strcmp(buffer, "quit") == 0) {
+            send_command(client_fd, buffer);
             break;
+        } else if (strncmp(buffer, "sgetfiles", strlen("sgetfiles")) == 0) {
+            char unzip_status[BUFFER_SIZE] = "";
+            int min_value, max_value;
+            sscanf(buffer, "%*s %d %d %s", &min_value, &max_value, unzip_status);
+            if (max_value < min_value) {
+                printf("Min value is not lesser than the Max value argument.\n");
+                continue;
+            }
+            send_command(client_fd, buffer);
+            int receive_status = receive_files(client_fd);
+            int unzip = strncmp(unzip_status, "-u", strlen("-u")) == 0 ? 1 : 0;
+            if (unzip && !receive_status) {
+                printf("Unzipping %s\n", TAR_FILE_NAME);
+                char system_call[BUFFER_SIZE] = "tar -xzvf";
+                strcat(system_call, TAR_FILE_NAME);
+                system(system_call);
+            }
+        } else if (strncmp(buffer, "dgetfiles", strlen("dgetfiles")) == 0) {
+            char unzip_status[BUFFER_SIZE] = "";
+            char min_date[BUFFER_SIZE];
+            char max_date[BUFFER_SIZE];
+            sscanf(buffer, "%*s %s %s", min_date, max_date);
+            if (validate_dates(min_date, max_date)) {
+                printf("Dates passed as arguments either do not follow the date format or min_date is after max_date\n");
+                continue;
+            }
+            send_command(client_fd, buffer);
+            int receive_status = receive_files(client_fd);
+            int unzip = strncmp(unzip_status, "-u", strlen("-u")) == 0 ? 1 : 0;
+            if (unzip && !receive_status) {
+                printf("Unzipping %s\n", TAR_FILE_NAME);
+                char system_call[BUFFER_SIZE] = "tar -xzvf";
+                strcat(system_call, TAR_FILE_NAME);
+                system(system_call);
+            }
+        } else {
+            send_command(client_fd, buffer);
+            // Receive response from server
+            // bzero(buffer, BUFFER_SIZE);
+            memset(buffer, 0, BUFFER_SIZE);
+            n = read(client_fd, buffer, BUFFER_SIZE - 1);
+            if (n < 0) {
+                perror("TCP Client - Read Error");
+                exit(EXIT_FAILURE);
+            }
+            if (n == 0) {
+                printf("Server disconnected.\n");
+                break;
+            }
+            buffer[n] = '\0';
+            printf("Server response: \n%s\n", buffer);
         }
-
-        // Receive response from server
-        bzero(buffer, BUFFER_SIZE);
-        n = read(client_fd, buffer, BUFFER_SIZE - 1);
-        if (n < 0) {
-            perror("TCP Client - Read Error");
-            exit(EXIT_FAILURE);
-        }
-        if (n == 0) {
-            printf("Server disconnected.\n");
-            break;
-        }
-        buffer[n] = '\0';
-        printf("Server response: %s\n", buffer);
     }
 
     close(client_fd);
--- server.c
+++ server.c
@@ -7,46 +7,181 @@
 #include <netinet/in.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <time.h>
 
 #define PORT 8080
 // Allowing server to listen to 10 clients at a time
 #define MAX_CLIENTS 10
 #define BUFFER_SIZE 1024
+#define TAR_FILE_NAME "server_temp.tar.gz"
 
-void processClient(int socket_fd) {
+// Function to Find the file in the server from /home/soham
+char* findfile(char* filename) {
+    char *ptr_client_str;
+    char str_appended[BUFFER_SIZE];
+    char command[BUFFER_SIZE];
+    sprintf(command, "find ~/ -name %s -printf '%%f|%%s|%%T@\\n' | head -n 1", filename);
+    FILE* fp = popen(command, "r");
+    char path[BUFFER_SIZE];
+    if (fgets(path, BUFFER_SIZE, fp) != NULL) {
+        printf("File Found.\n");
+        path[strcspn(path, "\n")] = 0; // Remove trailing newline
+        // Extract the filename, size, and date from the path string
+        char* filename_ptr = strtok(path, "|");
+        char* size_ptr = strtok(NULL, "|");
+        char* date_ptr = strtok(NULL, "|");
+
+        // Convert the size and date strings to integers
+        int size = atoi(size_ptr);
+        time_t date = atoi(date_ptr);
+
+        // Creating Filename string to be sent to client for displaying data
+        char print_filename[BUFFER_SIZE];
+        strcpy(print_filename, "File Name: ");
+        strcat(print_filename, filename_ptr);
+        strcat(print_filename, "\n");
+
+        // Creating File Size string to be sent to client for displaying data
+        char print_size[BUFFER_SIZE];
+        strcpy(print_size, "File Size: ");
+        strcat(print_size, size_ptr);
+        strcat(print_size, "\n");
+
+        // Creating File Created At string to be sent to client for displaying data
+        char print_created[BUFFER_SIZE];
+        strcpy(print_created, "File Created At: ");
+        strcat(print_created, ctime(&date));
+        strcat(print_created, "\n");
+
+        // Appending all necessary data of the file to be sent to client as a single string
+        strcpy(str_appended, print_filename);
+        strcat(str_appended, print_size);
+        strcat(str_appended, print_created);
+    } else {
+        printf("File not Found.\n");
+        strcpy(str_appended, "File not Found.\n");
+    }
+    pclose(fp);
+    ptr_client_str = str_appended;
+    return ptr_client_str;
+}
+
+// Function to handle sending stream of file
+void send_tar_file(int socket_fd) {
+    // Open the tar file in binary read mode
+    FILE* fp = fopen(TAR_FILE_NAME, "rb");
+    if (!fp) {
+        perror("Error opening tar file");
+        return;
+    }
+
+    // Send the tar file size to the client
+    fseek(fp, 0, SEEK_END);
+    long tar_file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    char size_buffer[BUFFER_SIZE];
+    sprintf(size_buffer, "%ld", tar_file_size);
+    if (send(socket_fd, size_buffer, strlen(size_buffer), 0) != strlen(size_buffer)) {
+        perror("Error sending tar file size to client");
+        fclose(fp);
+        return;
+    }
+    printf("Client Acknowledged.\n");
+    if (recv(socket_fd, size_buffer, BUFFER_SIZE, 0) == -1) {
+        perror("Error sending tar file size to client");
+        fclose(fp);
+        return;
+    }
+    printf("File of size %ld being sent.\n", tar_file_size);
+
+    // Sending the tar file contents to the client
+    char buffer[BUFFER_SIZE];
+    size_t n;
+    while ((n = fread(buffer, sizeof(char), BUFFER_SIZE, fp)) > 0) {
+        if (send(socket_fd, buffer, n, 0) != n) {
+            perror("Error sending tar file contents to client");
+            break;
+        }
+    }
+    printf("File sent successfully\n");
+    fclose(fp);
+}
+
+// TODO - If file is removed, there is error of "Error opening tar file"
+// Function to get the files in a range of size
+void sgetfiles(int socket_fd, int size1, int size2) {
+    char command[BUFFER_SIZE];
+    sprintf(command, "find ~/ -type f -size +%d -size -%d -print0 | tar -czvf %s --null -T -", size1, size2, TAR_FILE_NAME);
+    FILE* fp = popen(command, "r");
+    send_tar_file(socket_fd);
+}
+
+// Function to get the files in a range of dates
+void dgetfiles(int socket_fd, char* date1, char* date2) {
+    char command[BUFFER_SIZE];
+    sprintf(command, "find ~/ -type f -newermt \"%s\" ! -newermt \"%s\" -print0 | tar -czvf %s --null -T -", date1, date2, TAR_FILE_NAME);
+    FILE* fp = popen(command, "r");
+    send_tar_file(socket_fd);
+}
+
+int processClient(int socket_fd) {
+    char *result;
     char buffer[BUFFER_SIZE];
     int n, fd;
     while(1) {
+        result = NULL;
         bzero(buffer, BUFFER_SIZE);
         n = read(socket_fd, buffer, BUFFER_SIZE - 1);
         if (n < 0) {
             perror("TCP Server - Read Error");
-            exit(EXIT_FAILURE);
+            return 1;
         }
         if (n == 0) {
-            printf("Client disconnected.\n");
             break;
         }
         
         buffer[n] = '\0';
         printf("Command received: %s\n", buffer);
+        printf("Processing command...\n");
 
-        if (strcmp(buffer, "quit") == 0) {
+        if (strncmp(buffer, "findfile", strlen("findfile")) == 0) {
+            // Client asking for finding a file details
+            char filename[BUFFER_SIZE];
+            sscanf(buffer, "%*s %s", filename);
+            printf("Filename: %s\n", filename);
+            result = findfile(filename);
+        } else if (strncmp(buffer, "sgetfiles", strlen("sgetfiles")) == 0) {
+            int min_value, max_value;
+            sscanf(buffer, "%*s %d %d", &min_value, &max_value);
+            sgetfiles(socket_fd, min_value, max_value);
+            result = NULL;
+            continue;
+        } else if (strncmp(buffer, "dgetfiles", strlen("dgetfiles")) == 0) {
+            char min_date[BUFFER_SIZE];
+            char max_date[BUFFER_SIZE];
+            sscanf(buffer, "%*s %s %s", min_date, max_date);
+            dgetfiles(socket_fd, min_date, max_date);
+            result = NULL;
+            continue;
+        } else if (strcmp(buffer, "quit") == 0) {
+            // Client disconnecting from the server
+            printf("Client Quitting.\n");
             break;
+        } else {
+            // If there is no handler for handling client request, return the 
+            // command sent by the client back to it
+            result = buffer;
         }
-        else {
-            if (send(socket_fd, buffer, strlen(buffer), 0) != strlen(buffer)) {
-                perror("TCP Server - Send Error");
-                close(socket_fd);
-                continue;
-            }
-
-            printf("Sent response to client: %s\n", buffer);
+        if (send(socket_fd, result, strlen(result), 0) != strlen(result)) {
+            perror("TCP Server - Send Error");
+            close(socket_fd);
+            return 1;
         }
+        printf("Sent response to client: %s\n", result);
     }
-    printf("Client disconnected.\n");
     close(socket_fd);
-    exit(EXIT_SUCCESS);
+    return 0;
 }
 
 int main(int argc, char const *argv[]) {
@@ -122,8 +257,14 @@ int main(int argc, char const *argv[]) {
         if (childpid == 0) {
             // Child process
             close(server_fd);
-            processClient(client_fd);
-            exit(EXIT_SUCCESS);
+            int exit_status = processClient(client_fd);
+            if (exit_status == 0) {
+                printf("Client Disconnected with Success Code.\n");
+                exit(EXIT_SUCCESS);
+            } else {
+                printf("Client Disconnected with Error Code.\n");
+                exit(EXIT_FAILURE);
+            }
         } else {
             // Parent process
             num_of_clients++;
